a275bdbff54b42dd5c9a8dc4f851b4ad1994ba0b
Treesoid <70215358+Treesoid@users.noreply.github.com>
2022-05-20 20:47:16 +0200

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(missing_debug_implementations, rust_2018_idioms)]
+
+use anyhow::Result;
+
+use tracing::{info, Level};
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::util::SubscriberInitExt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    setup_tracing()?;
+    Ok(())
+}
+
+fn setup_tracing() -> Result<()> {
+    let subscriber = tracing_subscriber::fmt()
+    .with_max_level(Level::TRACE)
+    .with_thread_names(true)
+    .with_span_events(FmtSpan::FULL)
+    .finish();
+    subscriber.init();
+    Ok(())
 }
